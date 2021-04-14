05281d763ac8ee0e0d7051645c86cd75569592d3
Angel <sarrom95@gmail.com>
2021-04-14 03:33:12 +0200

    Merge REV_1.0.2
    Conflicts:
    	Mi_Reto_Git/src/Vehiculos/Vehiculo.java

---
+++ Ruleta/src/ut5/reto1/ruleta/mansilla/piña/RuletaMansillaPiña.java
@@ -0,0 +1,16 @@
+package ut5.reto1.ruleta.mansilla.piña;
+
+/**
+ *
+ * @author Ángel Mansilla y Carlos Piña
+ */
+public class RuletaMansillaPiña {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
