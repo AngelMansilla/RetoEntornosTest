84af03d6687f68e88427329e7a75e76e3a4e261c
Angel <sarrom95@gmail.com>
2021-04-14 13:37:54 +0200

    Crear primeros metodos

---
+++ Ruleta/src/ut5/reto1/ruleta/mansilla/piña/Codec.java
@@ -0,0 +1,42 @@
+package ut5.reto1.ruleta.mansilla.piña;
+
+/**
+ *
+ * @author Ángel Mansilla y Carlos Piña
+ */
+public class Codec {
+	private String panel;
+	private String panelCod;
+
+	public Codec(String panel) {
+		this.panel = panel;
+	}
+
+	public String getPanel() {
+		return panel;
+	}
+
+	public String getPanelCod() {
+		return panelCod;
+	}
+	
+	public void codificar(char c){
+		this.panelCod="";
+	}
+	
+	public void acierto(char c){
+		
+	}
+	
+	public void indicarConsonante(char c){
+		
+	}
+	
+	public void comprarVocal(char c){
+		
+	}
+	
+	public void resolverPanel(String s){
+		
+	}
+}
