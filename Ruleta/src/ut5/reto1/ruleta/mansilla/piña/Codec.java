14ed7b22f7ae7b56c8b4aa4e8ff746e807c110c4
Angel <sarrom95@gmail.com>
2021-04-15 12:03:32 +0200

    + acertado + comprobarPanel + codificar

--- Ruleta/src/ut5/reto1/ruleta/mansilla/piña/Codec.java
+++ Ruleta/src/ut5/reto1/ruleta/mansilla/piña/Codec.java
@@ -19,24 +19,42 @@ public class Codec {
 	public String getPanelCod() {
 		return panelCod;
 	}
-	
-	public void codificar(char c){
-		this.panelCod="";
+	/**
+	 * Ciframos el panel cambiando las letras por el caracter seleccionado para
+	 * cifrarlo.
+	 *
+	 * @param caracter El caracter a cambiar
+	 */
+	public void codificar(char caracter){
+		StringBuilder str = new StringBuilder(this.panel);
+		String aux = "" + caracter;//Creamos el String para poder utilizara el .replace
+		int longitud = this.panel.length();
+		for (int i = 0; i < longitud; i++) {
+			if (this.panel.charAt(i) != ' ') {
+				str.replace(i, i + 1, aux);
+			}
+		}
+		this.panelCod=str.toString();
 	}
 	
-	public void acierto(char c){
-		
+	public boolean acierto(char letra){
+		StringBuilder str = new StringBuilder(this.panelCod);
+		boolean correcto=false;
+		String aux;
+		for (int i = 0; i < this.panel.length(); i++) {
+			aux = "";
+			if (this.panel.toLowerCase().charAt(i) == letra
+				|| this.panel.toUpperCase().charAt(i) == letra) {
+				aux += this.panel.charAt(i);
+				str.replace(i, i + 1, aux);
+				correcto = true;
+			}
+		}
+		this.panelCod=str.toString();
+		return correcto;
 	}
 	
-	public void indicarConsonante(char c){
-		
-	}
-	
-	public void comprarVocal(char c){
-		
-	}
-	
-	public void resolverPanel(String s){
-		
+	public boolean comprobarPanel(String solucion){
+		return this.panel.equalsIgnoreCase(solucion);
 	}
 }
