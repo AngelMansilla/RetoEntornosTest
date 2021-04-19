afec5658161837d94ceb06fa557e2d10a93d2033
Angel <sarrom95@gmail.com>
2021-04-19 20:36:04 +0200

    + Libro.java

---
+++ UD5_Examern_Git/src/Libro.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sarro
+ */
+public class Libro {
+	protected String isbn;
+	protected String titulo;
+	protected String autor;
+}
