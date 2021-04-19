fbcbc1f650bbab517c3e1ac722405ad8d4d9ebfd
Angel <sarrom95@gmail.com>
2021-04-19 19:00:27 +0200

    + Vehiculo.java.

---
+++ Mi_Reto_Git2/src/Vehiculo.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sarro
+ */
+public class Vehiculo {
+	private final static double VELOCIDAD_MAXIMA = 90;
+}
